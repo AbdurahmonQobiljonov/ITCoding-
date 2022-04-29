9e689973a72eb09bd0219340b2cd2a64498b2a37
Asrbek <87761511+AsrbekCoder@users.noreply.github.com>
2022-04-29 09:28:56 +0500

    las change design

--- assets/Js/script.js
+++
@@ -1,7 +0,0 @@
-const burger = document.getElementById("burger");
-const controll = document.getElementById("controll");
-
-burger.addEventListener("click", () => {
-  burger.classList.toggle("active");
-  controll.classList.toggle("active");
-});
